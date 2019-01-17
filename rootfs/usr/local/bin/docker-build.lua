b4e2e279fe698d9abb369e7413d77dec1ff11c1e
agrozyme <agrozyme@gmail.com>
2019-01-18 04:28:53 +0800

    update

--- rootfs/usr/local/bin/docker-build.lua
+++ rootfs/usr/local/bin/docker-build.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/lua
+local core = require("docker-core")
 
 local function main()
-  local core = require("docker-core")
   -- core.run("apk add --no-cache $(apk search --no-cache -xq lua5.3-* | grep lua5.3- | grep -vF lua5.3-bit32)")
   core.run("apk add --no-cache su-exec tini curl")
   core.run("mv /etc/profile.d/color_prompt /etc/profile.d/color_prompt.sh")
