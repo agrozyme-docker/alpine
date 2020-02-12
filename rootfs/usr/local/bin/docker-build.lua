f2aa0645581bbcaf62f2e05b7d1902b38de612d5
agrozyme <agrozyme@gmail.com>
2020-02-12 17:20:58 +0800

    add busybox-suid

--- rootfs/usr/local/bin/docker-build.lua
+++ rootfs/usr/local/bin/docker-build.lua
@@ -3,7 +3,7 @@ local core = require("docker-core")
 
 local function main()
   -- core.run("apk add --no-cache $(apk search --no-cache -xq lua5.3-* | grep lua5.3- | grep -vF lua5.3-bit32)")
-  core.run("apk add --no-cache su-exec tini curl")
+  core.run("apk add --no-cache su-exec tini curl busybox-suid")
   core.run("mv /etc/profile.d/color_prompt /etc/profile.d/color_prompt.sh")
   core.update_user()
 end
